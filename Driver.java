006f8ef2d76ddbaf156ec41da4a1b576a641bf78
dhicks6 <77577974+dhicks6@users.noreply.github.com>
2021-03-06 10:35:54 -0700

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,1 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Driver {
+    public static void main(String[] args) {
+        Scanner keys = new Scanner(System.in);
+        System.out.println("Would you like to add a medication?" +
+         "Enter Y for yes and N for no.");
+         String selection = keys.next();
+         if (selection.equals("Y")) {
+             MedicationList.addEntry();
+         }
+         else {
+             System.out.println("Goodbye");
+         }
+    }
+}
 
