88339ef72e3bdf9e20e2ab4b3fbdfbdb67a2f3a0
dhicks6 <77577974+dhicks6@users.noreply.github.com>
2021-03-06 20:22:56 -0700

    Update PersonalInformation.java

--- PersonalInformation.java
+++ PersonalInformation.java
@@ -8,56 +8,53 @@ public class PersonalInformation {
 
     public static void addEntry() {
         // personal information format:Name, DOB, age, height, weight, blood type, emergency contacts
-        boolean keepGoing = true;
-        while (keepGoing) {
-            String[] personalData = new String[7];
-            System.out.println("What is your name?");
-            String PTName = keys.nextLine();
-            personalData[0] = PTName;
 
-            System.out.println("What is your date of birth? \n Please enter it in MM/DD/YYYY format");
-            String DOB = keys.nextLine();
-            personalData[1] = DOB;
+        String[] personalData = new String[7];
+        System.out.println("What is your name?");
+        String PTName = keys.nextLine();
+        personalData[0] = PTName;
 
-            System.out.println("What is your age?");
-            String age = keys.nextLine();
-            personalData[2] = age;
+        System.out.println("What is your date of birth? \nPlease enter it in MM/DD/YYYY format");
+        String DOB = keys.nextLine();
+        personalData[1] = DOB;
 
-            System.out.println("What is your height?");
-            String height = keys.nextLine();
-            personalData[3] = height;
+        System.out.println("What is your age?");
+        String age = keys.nextLine();
+        personalData[2] = age;
 
-            System.out.println("What is your weight?");
-            String weight = keys.nextLine();
-            personalData[4] = weight;
+        System.out.println("What is your height?");
+        String height = keys.nextLine();
+        personalData[3] = height;
 
-            System.out.println("Do you know your blood type? \n Enter Y for yes and N for no.");
-            String typeCheck = keys.nextLine();
-            if (typeCheck.equals("Y")) {
-                System.out.println("What is your blood type");
-                String bloodType = keys.nextLine();
-                personalData[5] = bloodType;
-            }
-            else {
-                personalData[5] = ("PT does not know their blood type");
-            }
-            System.out.println("Who is your emergency contact?");
-            String emergencyContactName = keys.nextLine();
-            System.out.println("What is your relation to " + emergencyContactName);
-            String emergencyContactRelation = keys.nextLine();
-            System.out.println("What is " + emergencyContactName +"'s phone number?");
-            String emergencyContactNumber = keys.nextLine();
-            String emergencyContact = ("Name: " + emergencyContactName + "," + " Relation: " + emergencyContactRelation + "," +
-                 " Phone number: " + emergencyContactNumber);
-            personalData[6] = emergencyContact;
+        System.out.println("What is your weight?");
+        String weight = keys.nextLine();
+        personalData[4] = weight;
 
-            personalInfoList.add(personalData);
-            keepGoing = false;
-            printList();
-            writeToFile();
+        System.out.println("Do you know your blood type? \nEnter Y for yes and N for no.");
+        String typeCheck = keys.nextLine();
+        if (typeCheck.equals("Y")) {
+            System.out.println("What is your blood type");
+            String bloodType = keys.nextLine();
+            personalData[5] = bloodType;
+        } else {
+            personalData[5] = ("PT does not know their blood type");
         }
+        System.out.println("Who is your emergency contact?");
+        String emergencyContactName = keys.nextLine();
+        System.out.println("What is your relation to " + emergencyContactName);
+        String emergencyContactRelation = keys.nextLine();
+        System.out.println("What is " + emergencyContactName + "'s phone number?");
+        String emergencyContactNumber = keys.nextLine();
+        String emergencyContact = ("Name: " + emergencyContactName + "," + " Relation: " + emergencyContactRelation + "," +
+                " Phone number: " + emergencyContactNumber);
+        personalData[6] = emergencyContact;
 
+        personalInfoList.add(personalData);
+        printList();
+        writeToFile();
     }
+
+
     // personal information format:Name, DOB, age, height, weight, blood type, emergency contacts
     public static void printList(){
         for (int i = 0; personalInfoList.size() > i; i++) {
